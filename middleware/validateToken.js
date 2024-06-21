d91f6732703cb2f19d49a73d4140039cc5e66140
AndileMasela <61992639+Andile-coder@users.noreply.github.com>
2024-06-21 21:53:13 +0200

    prod env 4

--- middleware/validateToken.js
+++ middleware/validateToken.js
@@ -6,7 +6,7 @@ const jwt = require("jsonwebtoken");
 const validateToken = asynHandler(async (req, res, next) => {
   let token;
   let authHeader = req.headers.Authorization || req.headers.authorization;
-
+  console.log("cookie token", req.cookies.token);
   if (authHeader && authHeader.startsWith("Bearer")) {
     //postman
     token = authHeader.split(" ")[1];
@@ -20,9 +20,9 @@ const validateToken = asynHandler(async (req, res, next) => {
     });
   } else if (req.cookies.token) {
     //browser
-    console.log("cookie token", req.cookies.token);
-    token = req.cookies.token;
     
+    token = req.cookies.token;
+
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
       if (err) {
         // res.status(constants.VALIDATION_ERROR);
